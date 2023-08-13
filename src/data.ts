29443dda2e1c7a27c129fb7ee6156562c34aa3ae
Aaron Lowe <cyberflameu@gmail.com>
2023-08-13 20:55:04 +1200

    various formatting changes, fixes, and content adjustments
    Signed-off-by: Aaron Lowe <cyberflameu@gmail.com>

--- src/data.ts
+++ src/data.ts
@@ -8,7 +8,7 @@ export const headerData = {
     },
     {
       text: 'Contact',
-      href: getPermalink('/contact'),
+      href: getPermalink('/contact-us'),
     },
     {
       text: 'Wiki',
@@ -38,7 +38,7 @@ export const footerData = {
         // { title: 'Fediverse - Mastodon', href: 'https://mastodon.tuhuratech.org.nz/@tuhuratech' },
         // { title: 'Fediverse - Pixelfed', href: 'https://pixelfed.tuhuratech.org.nz/@tuhuratech' },
         // { title: 'Matrix space', href: 'https://matrix.to/#/#home:matrix.tuhuratech.org.nz' },
-        { title: 'Wiki', href: 'https://wiki.tuhuratech.org.nz' },
+        // { title: 'Wiki', href: 'https://wiki.tuhuratech.org.nz' },
         { title: 'Discord', href: 'https://discord.gg/PNxh7cwKfQ' },
         // { title: 'Git', href: 'https://git.tuhuratech.org.nz/tuhuratech' },
         // { title: 'Identity proofs', href: 'https://keyoxide.org/contact@tuhuratech.org.nz' },
@@ -60,6 +60,7 @@ export const footerData = {
     //   href: 'https://pixelfed.tuhuratech.org.nz/@tuhuratech',
     // },
     // { label: 'Git', icon: 'tabler:git-merge', href: 'https://git.tuhuratech.org.nz/tuhuratech' },
+    { label: 'Discord', icon: 'simple-icons:discord', href: 'https://discord.gg/JEHncpzRcv' },
     { label: 'Facebook', icon: 'simple-icons:facebook', href: 'https://www.facebook.com/tuhuratech' },
     { label: 'LinkedIn', icon: 'simple-icons:linkedin', href: 'https://www.linkedin.com/company/tuhuratech' },
     { label: 'Instagram', icon: 'simple-icons:instagram', href: 'https://www.instagram.com/tuhuratech' },
