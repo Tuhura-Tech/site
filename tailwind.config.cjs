8cd40c1e37c2cad6c4897df05ac8c4701d38478f
Leon Bowie <leon@bowie-co.nz>
2023-08-13 17:38:52 +1200

    fix dark mode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,3 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
-const colors = require('tailwindcss/colors');
-
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
   theme: {
@@ -9,11 +6,6 @@ module.exports = {
         primary: '#6aa469',
         secondary: '#175b98',
         accent: 'var(--aw-color-accent)',
-      },
-      fontFamily: {
-        sans: ['var(--aw-font-sans)', ...defaultTheme.fontFamily.sans],
-        serif: ['var(--aw-font-serif)', ...defaultTheme.fontFamily.sans],
-        heading: ['var(--aw-font-heading)', ...defaultTheme.fontFamily.sans],
       },
     },
   },
