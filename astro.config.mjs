40a89081ef10196c5c6b065ff5eb4323bd03fb81
Leon Bowie <leon@bowie-co.nz>
2023-08-13 15:32:33 +1200

    general cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,60 +1,47 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-import { defineConfig } from 'astro/config';
+import { defineConfig, sharpImageService } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-import image from '@astrojs/image';
 import mdx from '@astrojs/mdx';
-import compress from 'astro-compress';
-import { readingTimeRemarkPlugin } from './src/utils/frontmatter.mjs';
 
 import { SITE } from './src/config.mjs';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  site: SITE.origin,
-  base: SITE.basePathname,
-  trailingSlash: SITE.trailingSlash ? 'always' : 'never',
+	site: SITE.origin,
+	base: SITE.basePathname,
+	trailingSlash: SITE.trailingSlash ? 'always' : 'never',
 
-  output: 'static',
+	output: 'static',
+	compressHTML: true,
 
-  markdown: {
-    remarkPlugins: [readingTimeRemarkPlugin],
+	integrations: [
+		tailwind({
+			config: {
+				applyBaseStyles: false,
+			},
+		}),
+		sitemap(),
+		mdx(),
+	],
+
+  experimental: {
+    assets: true
   },
 
-  integrations: [
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-    sitemap(),
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp',
-    }),
-    mdx(),
-
-    compress({
-      css: true,
-      html: {
-        removeAttributeQuotes: false,
-      },
-      img: false,
-      js: true,
-      svg: false,
-
-      logger: 1,
-    }),
-  ],
-
-  vite: {
-    resolve: {
-      alias: {
-        '~': path.resolve(__dirname, './src'),
-      },
-    },
+  image: {
+    service: sharpImageService(),
   },
+
+	vite: {
+		resolve: {
+			alias: {
+				'~': path.resolve(__dirname, './src'),
+			},
+		},
+	},
 });
--- src/utils/permalinks.ts
+++ src/utils/permalinks.ts
@@ -1,7 +1,14 @@
-import slugify from 'limax';
+import slugify from 'slug';
 
 import { SITE } from '~/config.mjs';
-import { trim } from '~/utils/utils';
+
+const trim = (str = '', ch?: string) => {
+  let start = 0,
+    end = str.length || 0;
+  while (start < end && str[start] === ch) ++start;
+  while (end > start && str[end - 1] === ch) --end;
+  return start > 0 || end < str.length ? str.substring(start, end) : str;
+};
 
 export const trimSlash = (s: string) => trim(trim(s, '/'));
 const createPath = (...params: string[]) => {
