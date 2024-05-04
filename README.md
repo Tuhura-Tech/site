56be35a54ca5c1f7533b7671cfae4eb0a52e8bf3
Leon Bowie <leon@bowie-co.nz>
2024-05-05 01:48:32 +1200

    feat: base rewrite

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# Website
-
-This is the website for Tūhura Tech ([tuhuratech.org.nz](https://tuhuratech.org.nz)); originally based on the AstroWind template.
+# Tuhura Tech Website
---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+const colors = require('tailwindcss/colors')
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+  ],
+  theme: {
+    colors: {
+      primary: '#6aa469',
+      secondary: '#175b98',
+      ...colors
+    },
+    extend: {},
+  },
+  plugins: [
+  ],
+  darkMode: 'media',
+};
