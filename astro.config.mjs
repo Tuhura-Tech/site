27d30b4ddd778cda0b4e9e02aa7733e12e32b608
Darkflame72 <leon@darkflame.dev>
2025-08-24 21:07:26 +1200

    fix: style

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,24 @@
-import sitemap from "@astrojs/sitemap";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "astro/config";
-import icon from "astro-icon";
+import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://tuhuratech.org.nz",
-	output: "static",
+	site: 'https://tuhuratech.org.nz',
+	output: 'static',
 	prefetch: true,
 
 	integrations: [sitemap(), icon()],
 
 	image: {
 		service: {
-			entrypoint: "astro/assets/services/sharp",
+			entrypoint: 'astro/assets/services/sharp',
 		},
 	},
 
 	vite: {
 		plugins: [tailwindcss()],
-		optimizeDeps: { include: ["leaflet"] },
+		optimizeDeps: { include: ['leaflet'] },
 	},
 });
--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,4 +1,4 @@
-import type { APIRoute } from "astro";
+import type { APIRoute } from 'astro';
 
 const getRobotsTxt = (sitemapURL: URL) => `\
 User-agent: *
@@ -8,6 +8,6 @@ Sitemap: ${sitemapURL.href}
 `;
 
 export const GET: APIRoute = ({ site }) => {
-	const sitemapURL = new URL("sitemap-index.xml", site);
+	const sitemapURL = new URL('sitemap-index.xml', site);
 	return new Response(getRobotsTxt(sitemapURL));
 };
